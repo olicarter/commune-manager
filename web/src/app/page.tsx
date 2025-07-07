c90f302d9198123b5cb70831a9d61de391c39ef1
Oli Carter <me@olicarter.dev>
2025-07-07 18:49:07 +0300

    Fix profile page TypeScript errors

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -7,6 +7,8 @@ export default function Page() {
       <Link href="/communities/new">Create Community</Link>
       <br />
       <Link href="/communities">View Communities</Link>
+      <br />
+      <Link href="/profile">Edit Profile</Link>
     </div>
   );
 }
